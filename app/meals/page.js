5c1ed7ffd5e123e8db26541340082b0300a3a64d
gahui-dev <dev2gahui@gmail.com>
2024-02-18 03:58:31 +0900

    Feat: 클라이언트 컴포넌트 사용

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,23 @@
-export default function Meals() {
-  return <h1>meals</h1>;
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        Delicious meals, created{' '}
+        <span className={classes.highlight}>by you</span>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share your favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meal={[]} />
+      </main>
+    </>
+  );
 }
