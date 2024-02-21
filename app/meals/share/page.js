a319ba9a94aed5aca87dd2ae0bb87802916cee1b
gahui-dev <dev2gahui@gmail.com>
2024-02-22 00:17:57 +0900

    Feat: fs, sql, slugify, xss 이미지 업로드 및 데이터베이스 저장

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,24 +1,10 @@
+'use client';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    // 오직 서버에서만 실행
-    'use server';
-
-    console.log(formData);
-    const meal = {
-      title: formData.get('title'),
-      summry: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email'),
-    };
-
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={classes.header}>
